757b94ebb73a493ce16b34b05dd254cac705f01a
Haris <haris.khan87@gmail.com>
2015-03-13 09:15:56 +0100

    multiple clocks

--- js/app.js
+++ js/app.js
@@ -1,16 +1,30 @@
-setInterval(updateClock, 1000);
-updateClock();
 
-function updateClock(){
-	var date = new Date();
-	var clock = document.getElementById('clock');
+var clock = new Clock('clock');
+var clock2 = new Clock('clock2', -60 ,"UTC");
 
-	clock.innerHTML = formatDigits(date.getHours()) + ":" + formatDigits(date.getMinutes()) + ":" +formatDigits(date.getSeconds());
-}
+function Clock(id,offset, label){
+	offset = offset || 0;
+	label = label || "";
+	var d = new Date();
+	this.offset= (offset + d.getTimezoneOffset())*60*100;
 
-function formatDigits(val){
-	if(val<10){
-		val = "0" + val;
-	}
-	return val
+	this.updateClock= function(){
+		var date = new Date();
+			date = new Date(this.offset + date.getTime());
+		var clock = document.getElementById(id);
+		clock.innerHTML = this.formatDigits(date.getHours()) + ":" + this.formatDigits(date.getMinutes()) + ":" + this.formatDigits(date.getSeconds()) + " "+ label;
+	};
+
+
+	this.formatDigits = function(val){
+		if(val<10){
+			val = "0" + val;
+		}
+		return val;
+	};
+
+	var that = this;
+	setInterval(function(){that.updateClock();}, 1000);
+
+	this.updateClock();
 }
